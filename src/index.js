a6cf65e8c49e982b13942e613f3afc6f626b1d12
maksgg <maks.zhovmir.97@gmail.com>
2024-09-23 21:41:38 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const bracketsMap = Object.fromEntries(bracketsConfig);
+  
+  for ( let char of str) {
+    if (bracketsMap[char]) {
+      if (char === bracketsMap[char] && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (!stack.length || bracketsMap[stack.pop()] !== char) {
+      return false;
+    }
+  }
+  return stack.length === 0;
 }
